5423c6b0bffd1d168719c3329c0be1a65936fada
Khokhlov Andrey <progeat@mail.ru>
2025-06-03 10:01:46 +0300

    feat: add page EpisodePage

---
+++ src/modules/episode/episode-info/episode-info.tsx
@@ -0,0 +1,18 @@
+import type { FC } from 'react';
+import type { EpisodeModel } from '../../../core/interfaces';
+
+type EpisodeInfoProps = {
+  episode: EpisodeModel;
+};
+
+export const EpisodeInfo: FC<EpisodeInfoProps> = ({ episode }) => {
+  return (
+    <div>
+      <h3>{episode.name}</h3>
+      <ul>
+        <li>air date: {episode.airDate}</li>
+        <li>episode: {episode.episode}</li>
+      </ul>
+    </div>
+  );
+};
