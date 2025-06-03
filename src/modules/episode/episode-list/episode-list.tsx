eabdb38d21bb50ce7f261bc51a8fb379b6890fe9
Khokhlov Andrey <progeat@mail.ru>
2025-06-03 09:54:25 +0300

    feat: add page EpisodesPage

---
+++ src/modules/episode/episode-list/episode-list.tsx
@@ -0,0 +1,21 @@
+import type { FC } from 'react';
+import { Link } from 'react-router-dom';
+import type { EpisodeModel } from '../../../core/interfaces';
+import { ROUTES } from '../../../core/enums';
+import styled from './episode-list.module.css';
+
+type EpisodeListProps = {
+  episodes: EpisodeModel[];
+};
+
+export const EpisodeList: FC<EpisodeListProps> = ({ episodes }) => {
+  return (
+    <ul className={styled.list}>
+      {episodes.map(({ id, name }) => (
+        <li className={styled.item} key={id}>
+          <Link to={`${ROUTES.EPISODES}/${id}`}>{name}</Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
---
+++ src/pages/episodes/episodes/page.tsx
@@ -0,0 +1,37 @@
+import type { FC } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import { EpisodeList } from '../../../modules/episode/episode-list';
+import { ControlPanel } from '../../../ui/control-panel';
+import type { EpisodeModel } from '../../../core/interfaces';
+import { SORT_DIRECTION } from '../../../core/enums';
+import { DtoToModelMapper } from '../../../core/mapers/dto-to-model.mappers';
+import { Sorter } from '../../../core/helpers/sorter.helpers';
+import episodesData from '../../../core/data/episode.json';
+import styled from './episodes-page.module.css';
+
+export const EpisodesPage: FC = () => {
+  const episodes = DtoToModelMapper.normolizeEpisodes(episodesData);
+
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const currentSortParam = searchParams.get('sort') ?? SORT_DIRECTION.ASC;
+  const sortedEpisodes = Sorter.sortByCreated(
+    episodes,
+    currentSortParam as SORT_DIRECTION
+  );
+
+  const handleSortChange = (newSortValue: SORT_DIRECTION) => {
+    setSearchParams({ sort: newSortValue });
+  };
+
+  return (
+    <div className={styled['episodes-page']}>
+      <h1>Episodes</h1>
+      <ControlPanel
+        currentSortParam={currentSortParam as SORT_DIRECTION}
+        handleSortChange={handleSortChange}
+      />
+      <EpisodeList episodes={sortedEpisodes as EpisodeModel[]} />
+    </div>
+  );
+};
