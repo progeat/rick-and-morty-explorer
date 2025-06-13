7c7e1c5adaee91b3132cc7feb49a44d818b1778c
Andrey Khokhlov <progeat@mail.ru>
2025-06-13 12:27:31 +0300

    feat: add a request episodes from the json-server

--- src/modules/episode/components/episode-list/episode-list.tsx
+++ src/modules/episode/components/episode-list/episode-list.tsx
@@ -1,14 +1,29 @@
 import type { FC } from 'react';
 import { Link } from 'react-router-dom';
 import type { EpisodeModel } from '../../../../core/interfaces';
+import { Loader } from '../../../../ui/loader';
 import { ROUTES } from '../../../../core/enums';
 import styled from './episode-list.module.css';
 
 type EpisodeListProps = {
   episodes: EpisodeModel[];
+  isLoading: boolean;
+  error: string | null;
 };
 
-export const EpisodeList: FC<EpisodeListProps> = ({ episodes }) => {
+export const EpisodeList: FC<EpisodeListProps> = ({
+  episodes,
+  isLoading,
+  error,
+}) => {
+  if (isLoading) {
+    return <Loader />;
+  }
+
+  if (error) {
+    return <p className={styled.error}>{error}</p>;
+  }
+
   return (
     <ul className={styled.list}>
       {episodes.map(({ id, name }) => (
