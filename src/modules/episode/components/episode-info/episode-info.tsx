076ef4c80e848b5bcc88076a4dc9cd0681f17651
Andrey Khokhlov <progeat@mail.ru>
2025-06-13 12:31:03 +0300

    feat: add a request episode from the json-server

--- src/modules/episode/components/episode-info/episode-info.tsx
+++ src/modules/episode/components/episode-info/episode-info.tsx
@@ -1,11 +1,27 @@
 import type { FC } from 'react';
 import type { EpisodeModel } from '../../../../core/interfaces';
+import { Loader } from '../../../../ui/loader';
+import styled from './episode-info.module.css';
 
 type EpisodeInfoProps = {
   episode: EpisodeModel;
+  isLoading: boolean;
+  error: string | null;
 };
 
-export const EpisodeInfo: FC<EpisodeInfoProps> = ({ episode }) => {
+export const EpisodeInfo: FC<EpisodeInfoProps> = ({
+  episode,
+  isLoading,
+  error,
+}) => {
+  if (isLoading || !episode) {
+    return <Loader />;
+  }
+
+  if (error) {
+    return <p className={styled.error}>{error}</p>;
+  }
+
   return (
     <div>
       <h3>{episode.name}</h3>
