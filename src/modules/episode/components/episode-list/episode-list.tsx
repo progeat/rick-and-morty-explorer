f78c6405676f43085a8d8a026a45d2c47bb69417
Khokhlov Andrey <progeat@mail.ru>
2025-06-17 13:54:51 +0300

    feat: use custom hook useRequestWithInfinityScroll for categories

--- src/modules/episode/components/episode-list/episode-list.tsx
+++ src/modules/episode/components/episode-list/episode-list.tsx
@@ -1,4 +1,4 @@
-import type { FC } from 'react';
+import type { FC, Ref } from 'react';
 import { Link } from 'react-router-dom';
 import type { EpisodeModel } from '../../../../core/interfaces';
 import { Loader } from '../../../../ui/loader';
@@ -9,28 +9,35 @@ type EpisodeListProps = {
   episodes: EpisodeModel[];
   isLoading: boolean;
   error: string | null;
+  hasMore: boolean;
+  lastNodeRef: Ref<HTMLLIElement> | null;
 };
 
-export const EpisodeList: FC<EpisodeListProps> = ({
-  episodes,
-  isLoading,
-  error,
-}) => {
-  if (isLoading) {
-    return <Loader />;
-  }
+export const EpisodeList: FC<EpisodeListProps> = (props) => {
+  const { episodes, isLoading, error, hasMore, lastNodeRef } = props;
 
   if (error) {
     return <p className={styled.error}>{error}</p>;
   }
 
   return (
-    <ul className={styled.list}>
-      {episodes.map(({ id, name }) => (
-        <li className={styled.item} key={id}>
-          <Link to={`${ROUTES.EPISODES}/${id}`}>{name}</Link>
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul className={styled.list}>
+        {episodes.map(({ id, name }, index) => (
+          <li
+            ref={episodes.length === index + 1 ? lastNodeRef : null}
+            className={styled.item}
+            key={id}
+          >
+            <Link to={`${ROUTES.EPISODES}/${id}`}>{name}</Link>
+          </li>
+        ))}
+      </ul>
+      {isLoading && hasMore && (
+        <div className={styled['loader-wrapper']}>
+          <Loader />
+        </div>
+      )}
+    </>
   );
 };
