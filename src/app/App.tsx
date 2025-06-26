9d43aaa5d00dc417b4c811441a928e39a8333d32
Andrey Khokhlov <progeat@mail.ru>
2025-06-26 11:54:23 +0300

    chore: install Mantine design

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,14 @@
 import type { FC } from 'react';
 import { AuthProvider } from './context';
 import { AppRouter } from './routes';
+import { MantineProvider } from '@mantine/core';
 
 export const App: FC = () => {
   return (
     <AuthProvider>
-      <AppRouter />
+      <MantineProvider>
+        <AppRouter />
+      </MantineProvider>
     </AuthProvider>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './app/styles/global.css';
+import '@mantine/core/styles.css';
 import { App } from './app';
 
 createRoot(document.getElementById('root')!).render(
